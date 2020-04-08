9af8aae7e3144d8846a501513a187493b86d5b5e
Marko Covic <markocovic1@gmail.com>
2020-04-08 10:57:09 +0200

    Added quotes, sources and yeras
    Array of objects

--- js/script.js
+++ js/script.js
@@ -10,7 +10,45 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+var quotes = [
+  {
+    quote: '“Well, maybe I don’t need your money. Wait, wait, I said maybe!”', 
+    source: 'Rachel Green',
+    year: '1994'
+  },
+  {
+    quote: '“Joey doesn’t share food!”', 
+    source: 'Joey Tribbiani',
+    year: '1995'
+  },
+  {
+    quote: '“Hi, I’m Chandler. I make jokes when I’m uncomfortable.”', 
+    source: 'Chandler Bing',
+    year: '1996'
+  },
+  {
+    quote: '“I wish I could, but I don’t want to.”', 
+    source: 'Phoebe Buffay',
+    year: '1997'
+  },
+  {
+    quote: '“You go down there, and you suck up to him. I mean, you suck like you’ve never sucked before.“', 
+    source: 'Monica Galler',
+    year: '1998'
+  },
+  {
+    quote: '“It tastes like feet!”', 
+    source: 'Ross Galler',
+    year: '1999'
+  },
+  {
+    quote: '“How you doin’?”', 
+    source: 'Joey Tribbiani',
+    year: '2000'
+  }
+];
 
+console.log(quotes);
 
 
 /***
